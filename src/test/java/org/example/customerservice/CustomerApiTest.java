661451db44509a909a08a4f85743026df59d8d0b
Daniel <daniel.dahl@yh.nackademin.se>
2026-09-23 05:15:09 -0300

    updated ci-cd deployment

--- src/test/java/org/example/customerservice/CustomerApiTest.java
+++ src/test/java/org/example/customerservice/CustomerApiTest.java
@@ -25,12 +25,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(initializers = CustomerApiTest.EnvLoader.class)
 public class CustomerApiTest {
 
-    static class EnvLoader implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    static class EnvLoader implements
+            ApplicationContextInitializer<ConfigurableApplicationContext> {
+
         @Override
-        public void initialize(ConfigurableApplicationContext applicationContext) {
-            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-            dotenv.entries().forEach(entry -> System.setProperty(
-                    entry.getKey(), entry.getValue()));
+        public void initialize(
+                ConfigurableApplicationContext applicationContext) {
+
+            Dotenv dotenv = Dotenv.configure()
+                    .ignoreIfMissing()
+                    .load();
+
+            dotenv.entries().forEach(entry -> {
+                String key = entry.getKey();
+
+                if (System.getenv(key) == null &&
+                        System.getProperty(key) == null) {
+
+                    System.setProperty(key, entry.getValue());
+                }
+            });
         }
     }
 
